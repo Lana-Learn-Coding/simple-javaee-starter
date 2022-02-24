6f09bb76b6fb7a12e58c01f459fe86fbc33bfa82
Lana <sitdownrightnow2552@gmail.com>
2022-02-25 03:22:05 +0700

    feat: add persistence config

---
+++ src/main/java/io/lana/ejb/HelloEntity.java
@@ -0,0 +1,31 @@
+package io.lana.ejb;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class HelloEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HelloEntity that = (HelloEntity) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
--- src/main/java/io/lana/ejb/HelloResource.java
+++ src/main/java/io/lana/ejb/HelloResource.java
@@ -3,15 +3,33 @@ package io.lana.ejb;
 import io.lana.ejb.lib.ViewableResource;
 import org.glassfish.jersey.server.mvc.Viewable;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.Collections;
 
 @Path("/hello-servlet")
 public class HelloResource extends ViewableResource {
+    @PersistenceContext
+    private EntityManager em;
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     public Viewable hello(@QueryParam("name") @DefaultValue("World") final String name) {
         return new Viewable("/hello.jsp", Collections.singletonMap("name", name));
+    }
+
+    @GET
+    @Path("/entity")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response hello() {
+        HelloEntity entity = new HelloEntity();
+        em.persist(entity);
+        em.flush();
+        return Response.ok(entity).build();
     }
 }
